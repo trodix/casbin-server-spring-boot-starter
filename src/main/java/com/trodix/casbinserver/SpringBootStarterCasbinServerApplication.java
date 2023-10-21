508b449be79a2028c734b3861a605b446d854012
Sebastien Vallet <sebastien.vallet89@gmail.com>
2023-10-21 09:23:21 +0200

    initial commit

---
+++ src/main/java/com/trodix/casbinserver/SpringBootStarterCasbinServerApplication.java
@@ -0,0 +1,13 @@
+package com.trodix.casbinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterCasbinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterCasbinServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trodix/casbinserver/SpringBootStarterCasbinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trodix.casbinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterCasbinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
